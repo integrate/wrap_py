3e1f3c8ecb7b92d7ebf88be03ed2d4b44d689bb3
integrate <vmatva@gmail.com>
2021-02-05 02:46:30 +0300

    Added server settings

--- wrap_py/_remote.py
+++ wrap_py/_remote.py
@@ -1,6 +1,11 @@
-import rpyc, subprocess, os, sys
+import rpyc, subprocess, os, sys, time
+from wrap_py import settings as st
+
+#server process
+proc = None
 
 def _launch_server():
+    global proc
     CREATE_NEW_CONSOLE =  0x00000010
     DETACHED_PROCESS = 0x00000008
     CREATE_NEW_PROCESS_GROUP = 0x00000200
@@ -10,17 +15,18 @@ def _launch_server():
     f = os.path.join(d, "server.py")
 
     ex_d = os.path.split(sys.executable)[0]
-    ex_f = os.path.join(ex_d, "pythonw")
+    ex_f = os.path.join(ex_d, "python")
 
-    subprocess.Popen([ex_f, f], creationflags=flags)
+    proc = subprocess.Popen([ex_f, f], creationflags=flags)
 
 
 def init_remote():
     try:
-        conn = rpyc.connect('127.0.0.1', 18861)
+        conn = rpyc.connect(st.SERVER_HOST_NAME, st.SERVER_PORT, keepalive=True)
     except:
         _launch_server()
-        conn = rpyc.connect('127.0.0.1', 18861)
+        time.sleep(1)
+        conn = rpyc.connect(st.SERVER_HOST_NAME, st.SERVER_PORT, keepalive=True)
 
     return conn
 
--- wrap_py/server.py
+++ wrap_py/server.py
@@ -1,21 +1,22 @@
 from wrap_py import server_service, server_class
-from wrap_py import wrap_base
+from wrap_py import wrap_base, settings as st
 
 import threading
 
 
 def start_server():
     # construct RPyC server
-    hostname = '127.0.0.1'
-    port = 18861
-    server = server_class.RpycServer(app=wrap_base.app, service=server_service.MainService, hostname=hostname, port=port)
+    server = server_class.RpycServer(app=wrap_base.app, service=server_service.MainService, hostname=st.SERVER_HOST_NAME, port=st.SERVER_PORT)
 
     #start in another thread
-    thr = threading.Thread(target=server.start, daemon=True)
+    thr = threading.Thread(target=server.start)
     thr.start()
 
     # start app in main thread
-    wrap_base.app.start_as_server()
+    try:
+        wrap_base.app.start_as_server()
+    finally:
+        server.close()
 
 
 if __name__ == '__main__':
