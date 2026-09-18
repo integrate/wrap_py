8a6021cc16b4b1bd8d372637ff63728f7b5f9939
Vyacheslav Matva <vmatva@gmail.com>
2026-09-18 03:02:09 +0300

    новая версия для нового питона. требует новую версию wrap_engine

--- scripts/upload.bat
+++ scripts/upload.bat
@@ -1,7 +1,7 @@
 set base_dir=%~dp0..
-set ver=0.2.3.1
+set ver=0.2.4.1
 
-set twine=%base_dir%\venv\scripts\twine.exe
+set twine=%base_dir%\.venv\scripts\twine.exe
 set ver_name=%base_dir%\dist\wrap_py-%ver%-py3-none-any.whl
 
 %twine% upload %ver_name%
